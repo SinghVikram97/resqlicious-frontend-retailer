c65fa2da9af44c9fd3d9fd8c7dbf96c1a0895b9e
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2024-06-28 23:28:21 -0400

    finished edit menu

--- src/components/EditMenu.jsx
+++ src/components/EditMenu.jsx
@@ -1,7 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useAuth } from "./AuthContext";
+import { useNavigate } from "react-router-dom";
+import { backend_url } from "../Constants";
 
 const EditMenu = () => {
+  const { isAuthenticated, user } = useAuth();
+  const navigate = useNavigate();
+  const [restaurant, setRestaurant] = useState(null);
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [newCategoryName, setNewCategoryName] = useState("");
@@ -11,32 +17,69 @@ const EditMenu = () => {
   });
 
   useEffect(() => {
-    const fetchCategories = async () => {
-      const token = localStorage.getItem("retailertoken");
-      try {
-        const response = await axios.get(
-          "http://localhost:8080/api/v1/categories",
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
+    const fetchRestaurant = async () => {
+      if (isAuthenticated && user) {
+        const token = localStorage.getItem("retailertoken");
+        try {
+          const response = await axios.get(
+            `${backend_url}/users/${user.userId}/restaurants`,
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+          if (response.data.length > 0) {
+            setRestaurant(response.data[0]);
+            await fetchMenuCategories(response.data[0].menuId, token); // Pass menuId to fetchMenuCategories
+          } else {
+            setRestaurant(null);
           }
-        );
-        setCategories(response.data);
-      } catch (error) {
-        console.error("Error fetching categories:", error);
+        } catch (error) {
+          console.error("Error fetching restaurant data:", error);
+          localStorage.removeItem("retailertoken");
+          navigate("/login");
+        }
       }
     };
 
-    fetchCategories();
-  }, []);
+    const fetchMenuCategories = async (menuId, token) => {
+      // Accept menuId and token as parameters
+      try {
+        const menuResponse = await axios.get(`${backend_url}/menus/${menuId}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const categoryIdList = menuResponse.data.categoryIdList;
+
+        const categoryFetchPromises = categoryIdList.map(async (categoryId) => {
+          const categoryResponse = await axios.get(
+            `${backend_url}/categories/${categoryId}`,
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+          return categoryResponse.data;
+        });
+        const fetchedCategories = await Promise.all(categoryFetchPromises);
+        setCategories(fetchedCategories);
+      } catch (error) {
+        console.error("Error fetching menu categories:", error);
+      }
+    };
+
+    fetchRestaurant();
+  }, [isAuthenticated, user, navigate]);
 
   const handleCategoryChange = async (e) => {
     const categoryId = parseInt(e.target.value);
     const token = localStorage.getItem("retailertoken");
     try {
       const response = await axios.get(
-        `http://localhost:8080/api/v1/categories/${categoryId}`,
+        `${backend_url}/categories/${categoryId}`,
         {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -47,7 +90,7 @@ const EditMenu = () => {
       const dishes = await Promise.all(
         category.dishIdList.map(async (dishId) => {
           const dishResponse = await axios.get(
-            `http://localhost:8080/api/v1/dishes/${dishId}`,
+            `${backend_url}/dishes/${dishId}`,
             {
               headers: {
                 Authorization: `Bearer ${token}`,
@@ -67,20 +110,12 @@ const EditMenu = () => {
     setNewCategoryName(e.target.value);
   };
 
-  const handleNewDishChange = (e) => {
-    const { name, value } = e.target;
-    setNewDish((prevDish) => ({
-      ...prevDish,
-      [name]: value,
-    }));
-  };
-
   const handleCreateCategory = async () => {
     const token = localStorage.getItem("retailertoken");
     try {
       const response = await axios.post(
-        "http://localhost:8080/api/v1/categories",
-        { name: newCategoryName, menuId: 1 },
+        `${backend_url}/categories`,
+        { name: newCategoryName, menuId: restaurant.menuId },
         {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -95,11 +130,19 @@ const EditMenu = () => {
     }
   };
 
+  const handleNewDishChange = (e) => {
+    const { name, value } = e.target;
+    setNewDish((prevDish) => ({
+      ...prevDish,
+      [name]: value,
+    }));
+  };
+
   const handleAddDish = async () => {
     const token = localStorage.getItem("retailertoken");
     try {
       const response = await axios.post(
-        `http://localhost:8080/api/v1/dishes`,
+        `${backend_url}/dishes`,
         {
           name: newDish.name,
           price: parseFloat(newDish.price),
@@ -128,15 +171,11 @@ const EditMenu = () => {
   const handleEditDish = async (dishId, updatedDish) => {
     const token = localStorage.getItem("retailertoken");
     try {
-      await axios.put(
-        `http://localhost:8080/api/v1/dishes/${dishId}`,
-        updatedDish,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
+      await axios.put(`${backend_url}/dishes/${dishId}`, updatedDish, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
       setSelectedCategory((prevCategory) => ({
         ...prevCategory,
         dishes: prevCategory.dishes.map((dish) =>
@@ -148,6 +187,83 @@ const EditMenu = () => {
       alert("Failed to edit dish. Please try again.");
     }
   };
+
+  const EditDishForm = ({ dish, onEdit }) => {
+    const [editedDish, setEditedDish] = useState({ ...dish });
+
+    const handleInputChange = (e) => {
+      const { name, value } = e.target;
+      setEditedDish((prevDish) => ({
+        ...prevDish,
+        [name]: value,
+      }));
+    };
+
+    const saveChanges = () => {
+      onEdit(dish.id, editedDish);
+    };
+
+    return (
+      <div className="mt-4 border-t pt-4">
+        <h3 className="text-lg font-semibold text-gray-900">Edit Dish</h3>
+        <div className="mt-2">
+          <label className="block text-sm font-medium text-gray-700">
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            value={editedDish.name}
+            onChange={handleInputChange}
+            className="mt-1 p-2 w-full border rounded"
+          />
+        </div>
+        <div className="mt-2">
+          <label className="block text-sm font-medium text-gray-700">
+            Price
+          </label>
+          <input
+            type="number"
+            name="price"
+            value={editedDish.price}
+            onChange={handleInputChange}
+            className="mt-1 p-2 w-full border rounded"
+          />
+        </div>
+        <button
+          onClick={saveChanges}
+          className="mt-2 w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
+        >
+          Save Changes
+        </button>
+      </div>
+    );
+  };
+
+  if (restaurant === null) {
+    return (
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center py-10 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-2xl bg-white rounded-lg shadow-lg overflow-hidden p-6 text-center">
+          <h2 className="text-2xl font-bold text-gray-900 mb-4">
+            No restaurant registered.
+          </h2>
+          <p className="text-gray-700 mb-6">
+            Please register your restaurant to manage the menu.
+          </p>
+          <button
+            onClick={() => navigate("/editprofile")}
+            className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+          >
+            Register Restaurant
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  if (!restaurant) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center py-10 px-4 sm:px-6 lg:px-8">
@@ -182,9 +298,7 @@ const EditMenu = () => {
             onChange={handleCategoryChange}
             className="mt-2 p-2 w-full border rounded"
           >
-            <option value="" disabled>
-              Select a category
-            </option>
+            <option value="">Select a Category</option>
             {categories.map((category) => (
               <option key={category.id} value={category.id}>
                 {category.name}
@@ -196,120 +310,56 @@ const EditMenu = () => {
         {selectedCategory && (
           <div>
             <h3 className="text-xl font-semibold text-gray-900">
-              Dishes in {selectedCategory.name}
+              Add New Dish
             </h3>
-            {selectedCategory.dishes && selectedCategory.dishes.length > 0 ? (
-              selectedCategory.dishes.map((dish) => (
-                <EditDishForm
-                  key={dish.id}
-                  dish={dish}
-                  onEdit={handleEditDish}
-                />
-              ))
-            ) : (
-              <p>No dishes found in this category.</p>
-            )}
-            <div className="mb-6">
-              <h3 className="text-xl font-semibold text-gray-900">Add Dish</h3>
+            <div className="mt-2">
+              <label className="block text-sm font-medium text-gray-700">
+                Name
+              </label>
               <input
                 type="text"
                 name="name"
                 value={newDish.name}
                 onChange={handleNewDishChange}
-                placeholder="Dish Name"
-                className="mt-2 p-2 w-full border rounded"
+                className="mt-1 p-2 w-full border rounded"
               />
+            </div>
+            <div className="mt-2">
+              <label className="block text-sm font-medium text-gray-700">
+                Price
+              </label>
               <input
                 type="number"
                 name="price"
                 value={newDish.price}
                 onChange={handleNewDishChange}
-                placeholder="Dish Price"
-                className="mt-2 p-2 w-full border rounded"
+                className="mt-1 p-2 w-full border rounded"
               />
-              <button
-                onClick={handleAddDish}
-                className="mt-2 w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
-              >
-                Add Dish
-              </button>
             </div>
+            <button
+              onClick={handleAddDish}
+              className="mt-2 w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
+            >
+              Add Dish
+            </button>
+
+            {selectedCategory.dishes && selectedCategory.dishes.length > 0 && (
+              <div className="mt-4">
+                <h3 className="text-xl font-semibold text-gray-900">
+                  Edit Dishes
+                </h3>
+                {selectedCategory.dishes.map((dish) => (
+                  <EditDishForm
+                    key={dish.id}
+                    dish={dish}
+                    onEdit={handleEditDish}
+                  />
+                ))}
+              </div>
+            )}
           </div>
         )}
       </div>
-    </div>
-  );
-};
-
-const EditDishForm = ({ dish, onEdit }) => {
-  const [editMode, setEditMode] = useState(false);
-  const [editedDish, setEditedDish] = useState(dish);
-
-  const handleEditToggle = () => {
-    setEditMode(!editMode);
-  };
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setEditedDish((prevDish) => ({
-      ...prevDish,
-      [name]: value,
-    }));
-  };
-
-  const handleEditSubmit = async () => {
-    try {
-      await onEdit(dish.id, editedDish);
-      setEditMode(false);
-    } catch (error) {
-      console.error("Error editing dish:", error);
-      alert("Failed to edit dish. Please try again.");
-    }
-  };
-
-  return (
-    <div className="mb-4">
-      {!editMode && (
-        <div className="flex items-center justify-between border-b border-gray-300 pb-2">
-          <div>
-            <p className="font-semibold">{dish.name}</p>
-            <p className="text-gray-600">${dish.price}</p>
-          </div>
-          <div>
-            <button onClick={handleEditToggle} className="text-blue-500">
-              Edit
-            </button>
-          </div>
-        </div>
-      )}
-      {editMode && (
-        <div className="flex items-center justify-between border-b border-gray-300 pb-2">
-          <div>
-            <input
-              type="text"
-              name="name"
-              value={editedDish.name}
-              onChange={handleInputChange}
-              className="border rounded p-2 mr-2"
-            />
-            <input
-              type="number"
-              name="price"
-              value={editedDish.price}
-              onChange={handleInputChange}
-              className="border rounded p-2 mr-2"
-            />
-          </div>
-          <div>
-            <button onClick={handleEditSubmit} className="text-green-500">
-              Save
-            </button>
-            <button onClick={handleEditToggle} className="text-gray-500 ml-2">
-              Cancel
-            </button>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
