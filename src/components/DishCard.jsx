af8af7cb70b3e70eb1770f8cd74c3fbe2df59348
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2024-08-03 22:44:56 -0400

    added dish image in dish card

--- src/components/DishCard.jsx
+++ src/components/DishCard.jsx
@@ -9,6 +9,15 @@ const DishCard = ({ dish }) => {
           <p className="text-gray-600 mb-2">${dish.price}</p>
           <p className="text-gray-700">{dish.description}</p>
           <p className="text-gray-700">Quantity: {dish.quantity}</p>
+          {dish.imageUrl ? (
+            <img
+              src={`http://localhost:50515/${dish.imageUrl}`}
+              alt={dish.name}
+              className="mb-4 w-32 h-32 object-cover"
+            />
+          ) : (
+            <p className="text-gray-700 mb-4">No image uploaded</p>
+          )}
         </div>
       </div>
     </div>
